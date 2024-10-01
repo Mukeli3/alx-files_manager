2f3d986584da0f54b5133a428c741935a029e12b
Mukeli3 <mukelicynthia20@gmail.com>
2024-10-01 15:50:36 +0000

    the API endpoints

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import AppController from '../controllers/AppController.js';
+
+
+const route = express.Router();
+
+// define stats and status routes
+route.get('/status', AppController.getStatus);
+route.get('/stats', AppController.getStats);
+
+export default route;
