caa2386c48a365fa8073250f7ca9925c5db5bb03
Mukeli3 <mukelicynthia20@gmail.com>
2024-10-02 00:35:56 +0000

    added new POST endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const route = express.Router();
 
 // define stats and status routes
 route.get('/status', AppController.getStatus);
 route.get('/stats', AppController.getStats);
+route.post('/users', UsersController.postNew);
 
 export default route;
